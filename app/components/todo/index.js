cf973d3d736ed01220bf85a610fb4a925059b19f
5h4hid5 <shahid.bracu@gmail.com>
2018-04-20 15:59:54 +0600

    Removed demo code

--- app/components/todo/index.js
+++
@@ -1,9 +0,0 @@
-const Todo = require('./todo.model');
-const todoController = require('./todo.controller');
-const todoRouter = require('./todo.route');
-
-module.exports = {
-  Todo,
-  todoController,
-  todoRouter,
-};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,9 @@
 const config = require('../config/config');
-const { todoRouter } = require('../components/todo');
 const { userRouter } = require('../components/user');
 const { authRouter } = require('../helpers/auth');
 
 const init = function (app) {
   app.use('/auth', authRouter);
-  app.use(`/${config.api}/todos`, todoRouter);
   app.use(`/${config.api}/users`, userRouter);
 };
 
